b222dae0fabcec0d9f8479c128f3a4e39e62ad32
DCGGAMES <54399916+DCGGAMES@users.noreply.github.com>
2019-08-22 13:30:31 +0200

    Add files via upload

---
+++ Player/MouseCamara.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCamara : MonoBehaviour
+{
+    private float inputH;
+    private  float inputV;
+    private float speed;
+    private Quaternion comprovador;
+    private Vector3 sig;
+    private Vector3 act;
+    private Quaternion final;
+
+    void Awake()
+    {
+
+        act = new Vector3(transform.rotation.x, transform.rotation.y, 0);
+
+    }
+   
+    void Start()
+    {
+       
+    }
+
+ 
+    void Update()
+    {
+        
+        inputH = Input.mousePosition.x;
+        inputV = Input.mousePosition.y;
+        comprovador = Quaternion.Euler(-inputV, inputH, 0);
+        sig = new Vector3(comprovador.x, comprovador.y, 0);
+        final = comprovador;
+      
+        transform.localRotation = final;
+
+    }
+    private void ConversorGrados()
+    {
+       act.y += inputH * speed;
+       act.x += inputV * speed;
+    }
+}
---
+++ Player/Player.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+
+
+public class Player: MonoBehaviour
+{
+
+    private float Range = 1.0f;
+    public float speed = 15.0F; 
+    public int jumpSpeed = 1; 
+    private bool saltando = false;
+    public float Angulo = 80.0f;
+    private bool caminando = false;
+    private bool activo = false;
+    
+
+    void Awake()
+    {
+
+        
+
+
+    }
+
+    void Start()
+    {
+      
+        Cursor.visible = false;
+
+
+
+    }
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            speed = 5.0F;
+
+        }
+        else
+        {
+          
+            speed = 3.0F;
+        }
+
+        Move(speed, jumpSpeed, saltando);
+       
+
+       
+
+    }
+
+
+
+
+    public void Move(float speed, float jumpSpeed, Boolean Saltando)
+    {
+
+
+      
+       
+
+        if (Input.GetKey(KeyCode.W))
+        {
+
+
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+            
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+          
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            
+
+
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+
+
+            saltando = true;
+
+
+            transform.Translate(transform.up * jumpSpeed);
+
+            if (Input.GetKeyUp(KeyCode.Space) && saltando)
+            {
+                transform.Translate(transform.up * jumpSpeed);
+
+
+            }
+
+            
+        }
+
+
+
+        
+
+
+    }
+    void OnCollisionStay(Collision col)
+    {
+        if (col.gameObject.tag == "suelo")
+        {
+            caminando = true;
+
+        }
+    }
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "suelo")
+        {
+            caminando = false;
+
+
+        }
+
+    }
+
+}
